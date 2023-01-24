48d0bab3472b525ee9f27e424f27fcb6dd33e0ce
md-hasan-miah <mdhasan01515@gmail.com>
2023-01-24 08:48:44 +0600

    first commit

---
+++ app/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render
+from django.views import View 
+from .models import Customer, Product, Cart, OrderPlaced
+from .forms import CustomerRegistrationForm, CustomerProfileForm
+from .forms import AuthenticationForm
+from django.contrib import messages
+from .forms import forms as LoginForm
+
+from app import forms
+
+
+class ProductView(View):
+ def get(self, request):
+    topwears = Product.objects.filter(category='TW')
+    bottomwears = Product.objects.filter(category='BW')
+    mobiles = Product.objects.filter(category='M')
+    laptop = Product.objects.filter(category='L')
+    return render(request, 'app/home.html',{'topwears': topwears, 'bottomwears': bottomwears, 'mobiles': mobiles, 'laptop': laptop})
+
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        return render(request, 'app/productdetail.html', {'product':product})
+
+def add_to_cart(request):
+ return render(request, 'app/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+#def profile(request):
+ #return render(request, 'app/profile.html')
+
+def address(request):
+ add = Customer.objects.filter(user=request.user)
+ return render(request, 'app/address.html', {'add': add, 'active': 'btn-primary'})
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+#def change_password(request):
+ #return render(request, 'app/changepassword.html')
+
+def mobile(request, data=None):
+ if data == None:
+     mobiles = Product.objects.filter(category='M')
+ elif data == 'Redmi' or data == 'Samsung':
+     mobiles = Product.objects.filter(category='M').filter(brand=data)
+ elif data == 'below':
+     mobiles = Product.objects.filter(category='M').filter(discounted_price__lt=10000)
+ elif data == 'above':
+     mobiles = Product.objects.filter(category='M').filter(discounted_price__gt=10000)    
+ return render(request, 'app/mobile.html', {'mobiles':mobiles})
+
+#class LoginView(View):
+    #def get(self, request):
+        #form = LoginForm()
+        #return render(request, 'app/login.html',{'form':form})
+
+def login(request):
+    form = AuthenticationForm()
+    return render(request, 'app/login.html', {'form':form})
+
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form':form})
+    
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Congratulation!! Registered Successfully')
+            form.save()
+        return render(request, 'app/customerregistration.html', {'form':form})
+
+def checkout(request):
+ return render(request, 'app/checkout.html')
+
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', {'form':form, 'active':'btn-primary'})
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr = request.user 
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']    
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+            reg = Customer(user=usr, name=name, locality=locality, city=city, state=state, zipcode=zipcode)
+            reg.save()
+            messages.success(request, 'congratulations! profile udated successfully')
+        return render(request, 'app/profile.html', {'form': form, 'active': 'btn-primary'})
+    
